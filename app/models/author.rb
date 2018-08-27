ba86245c9edf7afb54ea5b30651d1ff88bb45871
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-27 19:58:11 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+  validates :title, presence: true 
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validates :content, length: { minimum: 100}
 end
